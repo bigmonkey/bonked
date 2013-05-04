13f469b004a6874970c913184dd228c274c1d83f
haley <conway@xerpi.com>
2013-05-04 18:53:59 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bongko::Application.config.secret_key_base = 'f7aa8bc99a4843cc8d8a00e96416cc7fed7ec7048a7c886b8f4639e09ed9d2604cddfea16780ee4aa923b0b3187c9978b7c80e360a0ab07f50c64418247213d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bongko::Application.config.session_store :cookie_store, key: '_bongko_session'
